c42132eee788231322ff460a5852be1b67ae99ec
louaylh10 <hcinlouay@gmail.com>
2024-02-03 17:46:02 +0100

    first commit

---
+++ src/components/APropos.js
@@ -0,0 +1,24 @@
+import React, { useState, useRef, useEffect } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+function Propos() {
+  
+  return (
+    <div className="Apropos">
+<h1 className='Titre'>A Propos</h1>
+<p className='prp'>La société de Maintenance Industriels et Construction Métallique « Louay Industrie et Maintenance », est une S.A.R.L, créé en 2005, représentée par son Gérant Monsieur Samir Hcine.
+L.I.M vous propose des services de Maintenance Industrielle dans toute la Tunisie et à l'étranger.
+Nous avons l'honneur de vous présenter notre entreprise pour vous permettre d'avoir une appréciation des moyens humaines ,matériels ,techniques et financiers de manière à vous rendre compte de notre capacité à faire face à toute sorte de commandes se rapportant à notre secteur d'activité:
+<ul >
+  <li>Fabrication et montage charpente</li>
+  <li>Travaux mécanique industrielle</li>
+  <li>Fabrication et montage réservoir</li>
+  <li>Travaux tuyauterie</li>
+</ul>
+
+</p>
+
+    </div>
+  );
+}
+
+export default Propos;
